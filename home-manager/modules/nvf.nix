3dea6e47765c6479919fb57bf8cbfab8daeba6ed
Erwin de Vries <esdevries313@gmail.com>
2025-03-03 22:13:28 +0100

    spellcheck

--- home-manager/modules/nvf.nix
+++ home-manager/modules/nvf.nix
@@ -15,9 +15,20 @@
           style = "night";
         };
 
-        lsp = {
+        spellcheck = {
           enable = true;
+        };
+
+        lsp = {
           formatOnSave = true;
+          lspkind.enable = false;
+          lightbulb.enable = true;
+          lspsaga.enable = false;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          otter-nvim.enable = true;
+          lsplines.enable = true;
+          nvim-docs-view.enable = true;
         };
 
         languages = {
