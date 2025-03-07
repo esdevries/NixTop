657a6c054b4f4042e7ff84050b583e6d20cfee68
Erwin de Vries <esdevries313@gmail.com>
2025-03-07 15:03:18 +0100

    revert iwd

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -2,11 +2,9 @@
   networking = {
     hostName = "nixtop";
     useDHCP = lib.mkDefault true;
-    wireless.iwd.enable = lib.mkForce true;
 
     networkmanager = {
       enable = lib.mkDefault true;
-      wifi.backend = lib.mkForce "iwd";
     };
   };
 
